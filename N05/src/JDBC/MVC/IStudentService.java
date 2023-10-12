1744f9e5a4f1c9a5aa54ed6586caf307b535104d
nehakokare <nehakokare31@gmail.com>
2023-10-12 17:57:56 +0530

    JDBC Commit

---
+++ N05/src/JDBC/MVC/IStudentService.java
@@ -0,0 +1,27 @@
+package JDBC.MVC;
+
+
+import java.sql.SQLException;
+import java.util.List;
+//Program to define Service interface for JDBC Crud operation on Student
+public interface IStudentService {
+	void addStudent(Student student) throws SQLException ;
+	
+	List<Student> getAllStudents() throws SQLException ;
+	
+	List<Student> getStudentsByPer(float value) throws SQLException ;
+	
+	List<Student> getStudentsNameStartsWith(String str) throws SQLException ;
+	
+	int deleteAllStudents() throws SQLException ;
+	
+	int deleteByRollNo(int value)throws SQLException ;
+	
+	int updateStudentPer(float value) throws SQLException ;
+	
+	int updateStudentPerByRollNo(int rvalue, float pvalue ) throws SQLException ;
+
+}
+	 
+
+
